f2f33a2107394c8135f68b1133570fdba2b8b128
bon-pang <bonnieme667@gmail.com>
2025-06-26 18:15:08 -0600

    Initial Commit

---
+++ CourtCam/CourtCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtCamApp.swift
+//  CourtCam
+//
+//  Created by bpang24 on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CourtCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourtCamTests/CourtCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  CourtCamTests.swift
+//  CourtCamTests
+//
+//  Created by bpang24 on 6/26/25.
+//
+
+import Testing
+@testable import CourtCam
+
+struct CourtCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
